03f9406bb1722d671df5a30edf41f647f75dc206
Farouk-Data <farouk.echcharef@usmba.ac.ma>
2023-07-15 15:41:13 +0100

    controlers

--- learn.md
+++ learn.md
@@ -14,4 +14,16 @@
     => Modules are a way to group and encapsulates a set of capabilities (each module for a feature in a project)
     => Module for users, groups...
     => Dependency of modules(tree structure)
-    
+
+# 3_controllers:
+    (Using REST API)
+    => in order to have an api you need to have some kind of routes
+        e.g:
+            - GET /ninjas => return a collection of ninjas [] (a single route)
+            - GET /ninjas/:id => return a single ninja
+            - POST /ninjas => creates ninjas
+            - PUT /ninjas/:id => update
+            - DELETE /ninjas/:id => delete a ninja
+    => Controllers are in change of defining the paths and http methods
+    => @Controller('ninjas'): anything within this controller is gonna have that prefix ('ninjas')
+    => Controllers are responsible for handling incoming requests and returning responses to the client.
--- ninja-api/src/ninjas/ninjas.controller.ts
+++ ninja-api/src/ninjas/ninjas.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
 
+// /ninjas is the path
 @Controller('ninjas')
-export class NinjasController {}
+export class NinjasController {
+    //defining methods for each one of the routes
+    // in case of query strings
+    //- GET /ninjas?type=fast
+    @Get()
+    getNinjas(@Query('type') type:string){
+        return [{type}];
+    }
+
+    //- GET /ninjas/:id
+    @Get(':id')
+    // in order to parse the ':id' from the request so the logic in the method can work with it:
+    // we use the @Param decorator
+    getOneNinja(@Param('id') id: string){
+        return {id};
+    }
+
+    // POST /ninjas (test with postman)
+    @Post()
+    // Parse the request body
+    createNinja(){
+        return {};
+    }
+
+    // PUT /ninjas/:id
+    @Put(':id')
+    updateNinja(@Param('id') id: string){
+        return {id};
+    }
+
+    //DELETE /ninjas/:id
+    @Delete(':id')
+    removeNinja(@Param('id') id: string){
+        return {id};
+    }
+}
